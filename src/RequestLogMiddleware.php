16d5e2c6dbc0db810e063ce03dd6f276e9af04be
Vittorio Malaspina <vittorio.malaspina@gmail.com>
2021-03-17 10:25:43 +0100

    .

---
+++ src/RequestLogMiddleware.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace DaUnoANove\Lumen\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+class RequestLogMiddleware {
+    protected const REQUEST_LOG_CHANNEL = 'REQUEST_LOG_CHANNEL';
+
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    public function terminate($request, $response, string $log = null): void
+    {
+        if ( is_null($log) )
+        {
+            $log = $this->make($request, $response);
+        }
+
+        if ( is_null(env(self::REQUEST_LOG_CHANNEL)) )
+        {
+            Log::info($log);
+        } else {
+            Log::channel(env(self::REQUEST_LOG_CHANNEL))->info($log);
+        }
+    }
+
+    public function make($request, $response): string
+    {
+        switch ( true )
+        {
+            case str_contains($response->headers->get('Content-Type'), 'text/html'):
+                $res = $this->removeContent($response);
+                break;
+
+            default:
+                $res = (string)$response;
+                break;
+        }
+
+        return sprintf("\n>>>>>>>> REQUEST\n%s\n\n<<<<<<<< RESPONSE\n%s\n", (string)$request, $res);
+    }
+
+    protected function removeContent($obj): string
+    {
+        switch ( true )
+        {
+            case $obj instanceof Request;
+            case $obj instanceof Response;
+                return trim(str_replace($obj->getContent(), null, (string)$obj));
+                break;
+
+            default:
+                return '';
+                break;
+        }
+    }
+}
